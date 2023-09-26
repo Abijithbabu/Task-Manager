56b7f4be358112f42df565ce5f1095edbe23261a
Abhijith Babu <abijithb2002@gmail.com>
2023-09-26 21:03:31 +0530

    routes protected

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
+import { useSelector } from "react-redux";
 
 const Router = () => {
+  const auth = useSelector(store=>store.user)
+  console.error(auth)
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={auth ? <Home /> : <Navigate to={'/login'}/>} />
+        <Route path="/login" element={auth ? <Navigate to={'/'}/> : <Login />} />
       </Routes>
     </>
   );
